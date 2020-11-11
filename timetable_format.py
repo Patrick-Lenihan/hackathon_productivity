ebb4c0186356de2c758b110c60dab5ad0cc6a2c4
Eoin O'Sullivan <74117793+EoinOSullivanCS@users.noreply.github.com>
2020-11-11 12:31:24 +0000

    Update timetable_format.py

--- timetable_format.py
+++ timetable_format.py
@@ -19,7 +19,7 @@ c = conn.cursor()
 def create_Table():
     c.execute('CREATE TABLE IF NOT EXISTS timetables(times VARCHAR(10), Monday VARCHAR(10), Tuesday VARCHAR(10), Wednesday VARCHAR(10), Thursday VARCHAR(10),Friday VARCHAR(10), Saturday VARCHAR(10))')
 def data_Entry():
-    c.execute("INSERT INTO timetables(times,Monday,Tuesday,Wednesday,Thursday, Friday, Saturday) VALUES ('9.00', 'CS1117','','CS1106','','',''),('10.00', '','','','','',''),('11.00', '','','','','CS1117',''),('12.00', '','CS1115','','','CS1112',''),('13.00', 'CS1115','CS1106','CS1111','','',''),('14.00', 'CS1117','CS1111','','CS1115','',''),('15.00', '','CS1117','CS1113','CS1111','',''),('16.00', '','CS1117','CS1106','','',''),('17.00', '','','','CS1115','','')")                                                                                                  
+    c.execute("INSERT INTO timetables(times,Monday,Tuesday,Wednesday,Thursday, Friday, Saturday) VALUES ('9.00', 'CS1117','','CS1106','','',''),('10.00', '','','','','',''),('11.00', '','','','','CS1117',''),('12.00', '','CS1115','','','CS1112',''),('13.00', 'CS1115','CS1106','CS1111','','',''),('14.00', 'CS1117','CS1111','','CS1115','',''),('15.00', '','CS1117','CS1112','CS1111','',''),('16.00', '','CS1117','CS1106','','',''),('17.00', '','','','CS1115','','')")                                                                                                  
 create_Table()  
 data_Entry() 
 conn.commit()
